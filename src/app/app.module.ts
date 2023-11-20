f0f673aac5dea69183f7d34f2d4c7286d844315a
noumanalikhalid <nnoumanali44@gmail.com>
2023-11-20 13:48:54 +0500

    angular design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MatIconModule } from '@angular/material/icon';
+import { CardsComponent } from './cards/cards.component';
+import { CheckboxComponent } from './checkbox/checkbox.component';
+import { BodyComponent } from './body/body.component';
+import { DropdownComponent } from './dropdown/dropdown.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CardsComponent,
+    CheckboxComponent,
+    BodyComponent,
+    DropdownComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/body/body.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+// import voice from '../../assets/voice-over-1@2x.png'
+// import savetime from '../../assets/save-time-1.png';
+// import user from '../../assets/user-1.png';
+// import youtube from '../../assets/youtube-1.png';
+// import boost from '../../assets/boost-productivity-1.png';
+@Component({
+  selector: 'app-body',
+  templateUrl: './body.component.html',
+  styleUrls: ['./body.component.scss']
+})
+export class BodyComponent {
+  // voiceImage = voice;
+  // saveTimeImage = savetime;
+  // userImage = user;
+  // youtubeImage = youtube;
+  // boostImage = boost;
+
+}
